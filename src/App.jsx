f6767c5fcdd80ff78762b4a7dc51834fa16a6081
Gishyan <artgish1964@gmail.com>
2024-08-14 12:51:12 +0400

    canvas buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import MainPage from './Pages/MainPage';
 import Login from './Pages/Login/Login';
 import ForgotPassword from './Pages/Login/ForgotPassword';
 import ResetPassword from './Pages/Login/ResetPassword';
 import ProtectedMainLayout from './Components/ProtectedMainLayout';
 import useAuthStore from './store/useAuthStore';
 import Loading from './Components/Loading';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   const isLoggedIn = useAuthStore(state => state.isLoggedIn);
@@ -25,7 +25,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route element={<ProtectedMainLayout />}>
-          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<Dashboard />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {  
+  return (
+    <div className="text-3xl font-bold w-full h-24 flex items-center justify-between p-4 border-b-2">
+      <p>Hi Admin</p>
+    </div>
+  )
+}
