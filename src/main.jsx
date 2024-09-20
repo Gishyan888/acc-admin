d37c6f8506a5c662d6499e8673f5e3751e8fb4b2
Lalvar <mnacakanyan.lalvar@gmail.com>
2024-09-20 09:52:05 +0400

    add react-chartjs-2 dependency, comment charts and build

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,28 @@
 // import { StrictMode } from 'react'
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <App />
   // {/* </StrictMode>, */}
-);
+)
